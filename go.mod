d50531351dcbcb04cd9b1233d9896c99dd0aaa6c
Javed Khan <tuxcanfly@gmail.com>
2023-02-27 10:04:06 -0800

    wip

--- go.mod
+++ go.mod
@@ -6,11 +6,14 @@ require (
 	github.com/btcsuite/btcd v0.23.4
 	github.com/btcsuite/btcd/btcec/v2 v2.1.3
 	github.com/btcsuite/btcd/btcutil v1.1.3
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.1
+	github.com/davecgh/go-spew v1.1.1
 )
 
 require (
-	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.1 // indirect
 	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
+	github.com/btcsuite/go-socks v0.0.0-20170105172521-4720035b7bfd // indirect
+	github.com/btcsuite/websocket v0.0.0-20150119174127-31079b680792 // indirect
 	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
--- main.go
+++ main.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/btcsuite/btcd/btcec/v2/schnorr"
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/btcsuite/btcd/txscript"
 )
 
 // Sample data and keys for testing.
 var (
-	samplePrivateKey = "cS5LWK2aUKgP9LmvViG3m9HkfwjaEJpGVbrFHuGZKvW2ae3W9aUe"
+	bobPrivateKey      = "5JoQtsKQuH8hC9MyvfJAqo6qmKLm8ePYNucs7tPu2YxG12trzBt"
+	internalPrivateKey = "5JGgKfRy6vEcWBpLJV5FXUfMGNXzvdWzQHUM1rVLEUJfvZUSwvS"
+	walletPrivateKey   = "cP5ycVVC1ByiiJgHNdEedSfQ9h16cjCewywksKvQFVqCyzXbshzp"
 )
 
 func createTaprootAddress(embeddedData []byte) (string, error) {
+	// Sign the transaction with the sample keypair.
+	privKey, err := btcutil.DecodeWIF(bobPrivateKey)
+	if err != nil {
+		return "", fmt.Errorf("error decoding bob private key: %v", err)
+	}
+
+	pubKey := privKey.PrivKey.PubKey()
+
 	// Step 1: Construct the Taproot script with two leafs:
 	// left: empty
 	left, err := txscript.NewScriptBuilder().Script()
@@ -28,6 +40,8 @@ func createTaprootAddress(embeddedData []byte) (string, error) {
 	builder.AddOp(txscript.OP_IF)
 	builder.AddData(embeddedData)
 	builder.AddOp(txscript.OP_ENDIF)
+	builder.AddData(pubKey.SerializeCompressed()[1:])
+	builder.AddOp(txscript.OP_CHECKSIG)
 	right, err := builder.Script()
 	if err != nil {
 		return "", fmt.Errorf("error constructing Taproot script: %v", err)
@@ -39,21 +53,20 @@ func createTaprootAddress(embeddedData []byte) (string, error) {
 		txscript.NewTapLeaf(txscript.BaseLeafVersion, right),
 	)
 
-	// Sign the transaction with the sample keypair.
-	privKey, err := btcutil.DecodeWIF(samplePrivateKey)
+	hash := tapBranch.TapHash()
+
+	internalPrivKey, err := btcutil.DecodeWIF(internalPrivateKey)
 	if err != nil {
-		return "", fmt.Errorf("error decoding sample private key: %v", err)
+		return "", fmt.Errorf("error decoding internal private key: %v", err)
 	}
 
-	pubKey := privKey.PrivKey.PubKey()
+	internalPubKey := internalPrivKey.PrivKey.PubKey()
 
-	hash := tapBranch.TapHash()
-	tweakedPubkey := txscript.ComputeTaprootOutputKey(pubKey, hash.CloneBytes())
+	tweakedPubkey := txscript.ComputeTaprootOutputKey(internalPubKey, hash.CloneBytes())
 
 	// Step 3: Generate the Bech32 address.
 	address, err := btcutil.NewAddressTaproot(
-		schnorr.SerializePubKey(tweakedPubkey),
-		&chaincfg.RegressionNetParams)
+		schnorr.SerializePubKey(tweakedPubkey), &chaincfg.RegressionNetParams)
 	if err != nil {
 		return "", fmt.Errorf("error encoding Taproot address: %v", err)
 	}
@@ -61,8 +74,40 @@ func createTaprootAddress(embeddedData []byte) (string, error) {
 	return address.String(), nil
 }
 
-func commitTx() {
-	// TODO
+func commitTx(addr string) error {
+	// Set up the connection to the btcd RPC server.
+	connCfg := &rpcclient.ConnConfig{
+		Host:         "localhost:18332",
+		User:         "rpcuser",
+		Pass:         "rpcpass",
+		HTTPPostMode: true,
+		DisableTLS:   true,
+	}
+	client, err := rpcclient.New(connCfg, nil)
+	if err != nil {
+		return fmt.Errorf("error creating btcd RPC client: %v", err)
+	}
+	defer client.Shutdown()
+
+	// Create a transaction that sends 0.0001 BTC to the given address.
+	address, err := btcutil.DecodeAddress(addr, &chaincfg.RegressionNetParams)
+	if err != nil {
+		return fmt.Errorf("error decoding recipient address: %v", err)
+	}
+
+	amount, err := btcutil.NewAmount(0.001)
+	if err != nil {
+		return fmt.Errorf("error creating new amount: %v", err)
+	}
+
+	hash, err := client.SendToAddress(address, amount)
+	if err != nil {
+		return fmt.Errorf("error sending to address: %v", err)
+	}
+
+	// Print the transaction hash.
+	log.Printf("Transaction sent: %s", hash.String())
+	return nil
 }
 
 func revealTx() {
@@ -71,11 +116,15 @@ func revealTx() {
 
 func main() {
 	// Example usage
-	embeddedData := []byte("hello world")
+	embeddedData := []byte("00")
 	address, err := createTaprootAddress(embeddedData)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-	fmt.Println("Taproot address:", address)
+	err = commitTx(address)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 }
