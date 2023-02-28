eedbab82eb0c8a541450da72583b64f9af517ad8
Javed Khan <tuxcanfly@gmail.com>
2023-02-28 05:06:48 -0800

    refactor: export relayer

--- relayer.go
+++ relayer.go
@@ -1,6 +1,7 @@
 package rollkitbtc
 
 import (
+	"bytes"
 	"fmt"
 
 	"github.com/btcsuite/btcd/btcec/v2"
@@ -13,6 +14,65 @@ import (
 	"github.com/btcsuite/btcd/wire"
 )
 
+// PROTOCOL_ID allows data identification by looking at the first few bytes
+var PROTOCOL_ID = []byte{0x72, 0x6f, 0x6c, 0x6c}
+
+// Sample data and keys for testing.
+// bob key pair is used for signing reveal tx
+// internal key pair is used for tweaking
+var (
+	bobPrivateKey      = "5JoQtsKQuH8hC9MyvfJAqo6qmKLm8ePYNucs7tPu2YxG12trzBt"
+	internalPrivateKey = "5JGgKfRy6vEcWBpLJV5FXUfMGNXzvdWzQHUM1rVLEUJfvZUSwvS"
+)
+
+// createTaprootAddress returns an address committing to a Taproot script with
+// a single leaf containing the spend path with the script:
+// <embedded data> OP_DROP <pubkey> OP_CHECKSIG
+func createTaprootAddress(embeddedData []byte) (string, error) {
+	privKey, err := btcutil.DecodeWIF(bobPrivateKey)
+	if err != nil {
+		return "", fmt.Errorf("error decoding bob private key: %v", err)
+	}
+
+	pubKey := privKey.PrivKey.PubKey()
+
+	// Step 1: Construct the Taproot script with one leaf.
+	builder := txscript.NewScriptBuilder()
+	builder.AddData(embeddedData)
+	builder.AddOp(txscript.OP_DROP)
+	builder.AddData(schnorr.SerializePubKey(pubKey))
+	builder.AddOp(txscript.OP_CHECKSIG)
+	pkScript, err := builder.Script()
+	if err != nil {
+		return "", fmt.Errorf("error building script: %v", err)
+	}
+
+	tapLeaf := txscript.NewBaseTapLeaf(pkScript)
+	tapScriptTree := txscript.AssembleTaprootScriptTree(tapLeaf)
+
+	internalPrivKey, err := btcutil.DecodeWIF(internalPrivateKey)
+	if err != nil {
+		return "", fmt.Errorf("error decoding internal private key: %v", err)
+	}
+
+	internalPubKey := internalPrivKey.PrivKey.PubKey()
+
+	// Step 2: Generate the Taproot tree.
+	tapScriptRootHash := tapScriptTree.RootNode.TapHash()
+	outputKey := txscript.ComputeTaprootOutputKey(
+		internalPubKey, tapScriptRootHash[:],
+	)
+
+	// Step 3: Generate the Bech32m address.
+	address, err := btcutil.NewAddressTaproot(
+		schnorr.SerializePubKey(outputKey), &chaincfg.RegressionNetParams)
+	if err != nil {
+		return "", fmt.Errorf("error encoding Taproot address: %v", err)
+	}
+
+	return address.String(), nil
+}
+
 // payToTaprootScript creates a pk script for a pay-to-taproot output key.
 func payToTaprootScript(taprootKey *btcec.PublicKey) ([]byte, error) {
 	return txscript.NewScriptBuilder().
@@ -21,14 +81,14 @@ func payToTaprootScript(taprootKey *btcec.PublicKey) ([]byte, error) {
 		Script()
 }
 
-// relayer is a bitcoin client wrapper which provides reader and writer methods
+// Relayer is a bitcoin client wrapper which provides reader and writer methods
 // to write binary blobs to the blockchain.
-type relayer struct {
+type Relayer struct {
 	client *rpcclient.Client
 }
 
 // close shuts down the client.
-func (r relayer) close() {
+func (r Relayer) close() {
 	r.client.Shutdown()
 }
 
@@ -36,7 +96,7 @@ func (r relayer) close() {
 // output is only spendable by posting the embedded data on chain, as part of
 // the script satisfying the tapscript spend path that commits to the data. It
 // returns the hash of the commit transaction and error, if any.
-func (r relayer) commitTx(addr string) (*chainhash.Hash, error) {
+func (r Relayer) commitTx(addr string) (*chainhash.Hash, error) {
 	// Create a transaction that sends 0.001 BTC to the given address.
 	address, err := btcutil.DecodeAddress(addr, &chaincfg.RegressionNetParams)
 	if err != nil {
@@ -59,7 +119,7 @@ func (r relayer) commitTx(addr string) (*chainhash.Hash, error) {
 // revealTx spends the output from the commit transaction and as part of the
 // script satisfying the tapscript spend path, posts the embedded data on
 // chain. It returns the hash of the reveal transaction and error, if any.
-func (r relayer) revealTx(embeddedData []byte, commitHash *chainhash.Hash) (*chainhash.Hash, error) {
+func (r Relayer) revealTx(embeddedData []byte, commitHash *chainhash.Hash) (*chainhash.Hash, error) {
 	rawCommitTx, err := r.client.GetRawTransaction(commitHash)
 	if err != nil {
 		return nil, fmt.Errorf("error getting raw commit tx: %v", err)
@@ -163,24 +223,71 @@ func (r relayer) revealTx(embeddedData []byte, commitHash *chainhash.Hash) (*cha
 	return hash, nil
 }
 
-// newRelayer returns a new relayer. It can error if there's an RPC connection
+type Config struct {
+	Host         string
+	User         string
+	Pass         string
+	HTTPPostMode bool
+	DisableTLS   bool
+}
+
+// NewRelayer returns a new relayer. It can error if there's an RPC connection
 // error with the connection config.
-func newRelayer() (*relayer, error) {
+func NewRelayer(config Config) (*Relayer, error) {
 	// Set up the connection to the btcd RPC server.
 	// NOTE: for testing bitcoind can be used in regtest with the following params -
 	// bitcoind -chain=regtest -rpcport=18332 -rpcuser=rpcuser -rpcpassword=rpcpass -fallbackfee=0.000001
 	connCfg := &rpcclient.ConnConfig{
-		Host:         "localhost:18332",
-		User:         "rpcuser",
-		Pass:         "rpcpass",
-		HTTPPostMode: true,
-		DisableTLS:   true,
+		Host:         config.Host,
+		User:         config.User,
+		Pass:         config.Pass,
+		HTTPPostMode: config.HTTPPostMode,
+		DisableTLS:   config.DisableTLS,
 	}
 	client, err := rpcclient.New(connCfg, nil)
 	if err != nil {
 		return nil, fmt.Errorf("error creating btcd RPC client: %v", err)
 	}
-	return &relayer{
+	return &Relayer{
 		client: client,
 	}, nil
+}
+
+func (r Relayer) Read(height uint64) ([][]byte, error) {
+	hash, err := r.client.GetBlockHash(int64(height))
+	if err != nil {
+		return nil, err
+	}
+	block, err := r.client.GetBlock(hash)
+	if err != nil {
+		return nil, err
+	}
+
+	var data [][]byte
+	for _, tx := range block.Transactions {
+		if len(tx.TxIn[0].Witness) > 1 {
+			witness := tx.TxIn[0].Witness[1]
+			size := uint(witness[0])
+			if bytes.HasPrefix(witness[1:], PROTOCOL_ID) {
+				data = append(data, witness[1:size+1])
+			}
+		}
+	}
+	return data, nil
+}
+
+func (r Relayer) Write(data []byte) (*chainhash.Hash, error) {
+	address, err := createTaprootAddress(data)
+	if err != nil {
+		return nil, err
+	}
+	hash, err := r.commitTx(address)
+	if err != nil {
+		return nil, err
+	}
+	hash, err = r.revealTx(data, hash)
+	if err != nil {
+		return nil, err
+	}
+	return hash, nil
 }
---
+++ relayer_test.go
@@ -0,0 +1,73 @@
+package rollkitbtc_test
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	rollkitbtc "github.com/rollkit/rollkit-btc"
+)
+
+// ExampleRelayer_Write tests that writing data to the blockchain works as
+// expected.
+func ExampleRelayer_Write() {
+	// Example usage
+	relayer, err := rollkitbtc.NewRelayer(rollkitbtc.Config{
+		Host:         "localhost:18332",
+		User:         "rpcuser",
+		Pass:         "rpcpass",
+		HTTPPostMode: true,
+		DisableTLS:   true,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Writing...")
+	_, err = relayer.Write([]byte("rollkit-btc: gm"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("done")
+	// Output: Writing...
+	// done
+}
+
+// ExampleRelayer_Read tests that reading data from the blockchain works as
+// expected.
+func ExampleRelayer_Read() {
+	// Example usage
+	relayer, err := rollkitbtc.NewRelayer(rollkitbtc.Config{
+		Host:         "localhost:18332",
+		User:         "rpcuser",
+		Pass:         "rpcpass",
+		HTTPPostMode: true,
+		DisableTLS:   true,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = relayer.Write([]byte("rollkit-btc: gm"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// TODO: either mock or generate block
+	// We're assuming the prev tx was mined at height 146
+	height := uint64(146)
+	blobs, err := relayer.Read(height)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, blob := range blobs {
+		got, err := hex.DecodeString(fmt.Sprintf("%x", blob))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(got))
+	}
+	// Output: rollkit-btc: gm
+}
